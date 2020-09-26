df940e1299508ecf4c2db8e554df815815f5aa9f
Mikhail Ilyushin <ilyushin_misha@mail.ru>
2020-09-26 20:27:10 +0300

    Старт рефакторинга для MVVM паттерна

---
+++ RSPO_UP_3/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSPO_UP_3.ViewModel.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool _disposed;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool SetValue<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing || _disposed) return;
+            _disposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ RSPO_UP_3/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSPO_UP_3.ViewModel.Base;
+
+namespace RSPO_UP_3.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
