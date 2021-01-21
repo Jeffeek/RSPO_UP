790297b4fe9501b51d8e786371a0560e1556f23b
Mikhail Ilyushin <ilyushin_misha@mail.ru>
2021-01-21 19:31:00 +0300

    Добавлено решение для одного из заданий в 12

--- RSPO_UP_12/Program.cs
+++ RSPO_UP_12/Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using CyberMath.Structures.Matrices.JaggedMatrix;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using СyberMath.Structures.Matrices.Extensions;
-using СyberMath.Structures.Matrices.Matrix;
-using СyberMath.Structures.Matrices;
-using СyberMath.Structures.Matrices.JaggedMatrix;
+using CyberMath.Structures.Matrices.Extensions;
 
 namespace RSPO_UP_12
 {
@@ -23,10 +21,17 @@ namespace RSPO_UP_12
 	        //var B = A.Select(x => x.Aggregate((acc, item) => acc * item)).ToArray();
 
 	        //3
-	        var text = File.ReadAllLines($"{Directory.GetCurrentDirectory()}\\jaggedMatrix.txt");
-	        var matrix = new int[text.Length][];
-	        for(var i = 0; i < text.Length; i++)
-		        matrix[i] = new int[text[i].Split().Length];
+	  //      var text = File.ReadAllLines($"{Directory.GetCurrentDirectory()}\\jaggedMatrix.txt");
+	  //      var jaggedMatrix = new JuggedMatrix<int>(text.Length, text.Select(x => x.Split().Length));
+			//jaggedMatrix.FillRandomly();
+	  //      var answers = jaggedMatrix.Select((x, i) => i % 2 != 0 ? x.Where(_ => _ > 0).Max() : -1)
+	  //                                .Where(x => x < 0)
+	  //                                .Average();
+
+			//4
+			//var converter = new PostfixToInfixConverter();
+			//var input = "xy+v*ut*";
+			//Console.WriteLine(converter.Convert(input));
         }
 
         private static int Sum(int[] array) => InternalSumRecursion(array, 0, 0);
