f2a87498175a9549b619f9b71462f76113d7ae5b
Mikhail Ilyushin <mishamine26@mail.ru>
2021-02-04 09:35:13 +0300

    12 доделка

---
+++ RSPO_UP_12/IntParse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSPO_UP_12
+{
+    public static class IntParse
+    {
+	    public static int Mul(this int first, int second) => first * second;
+    }
+}
--- RSPO_UP_12/Program.cs
+++ RSPO_UP_12/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using CyberMath.Extensions.Int32;
 using CyberMath.Structures.Matrices.Extensions;
@@ -15,79 +16,113 @@ namespace RSPO_UP_12
 	{
 		private static void Main(string[] args)
 		{
-			//1
-			//int[] arr = {1, 2, 3, 4, 5};
-			//var sum = Sum(arr);
+            //1
+            //int[] arr = {1, 2, 3, 4, 5};
+            //var sum = Sum(arr);
 
-			//2
-			//var A = new Matrix<int>(5, 2);
-			//A.FillRandomly(0, 10);
-			//var B = A.Select(x => x.Aggregate((acc, item) => acc * item)).ToArray();
+            //2
+            //var A = new Matrix<int>(5, 2);
+            //A.FillRandomly(0, 10);
+            //var B = A.Select(x => x.Aggregate((acc, item) => acc * item)).ToArray();
 
-			//3
-			//      var text = File.ReadAllLines($"{Directory.GetCurrentDirectory()}\\jaggedMatrix.txt");
-			//      var jaggedMatrix = new JuggedMatrix<int>(text.Length, text.Select(x => x.Split().Length));
-			//jaggedMatrix.FillRandomly();
-			//      var answers = jaggedMatrix.Select((x, i) => i % 2 != 0 ? x.Where(_ => _ > 0).Max() : -1)
-			//                                .Where(x => x < 0)
-			//                                .Average();
+            //3
+            //      var text = File.ReadAllLines($"{Directory.GetCurrentDirectory()}\\jaggedMatrix.txt");
+            //      var jaggedMatrix = new JuggedMatrix<int>(text.Length, text.Select(x => x.Split().Length));
+            //jaggedMatrix.FillRandomly();
+            //      var answers = jaggedMatrix.Select((x, i) => i % 2 != 0 ? x.Where(_ => _ > 0).Max() : -1)
+            //                                .Where(x => x < 0)
+            //                                .Average();
 
-			//4
-			//var converter = new PostfixToInfixConverter();
-			//var input = "ABC*EF-/+";
-			//Console.WriteLine(converter.Convert(input));
+            //4
+            //var converter = new PostfixToInfixConverter();
+            //var input = "ABC*EF-/+";
+            //Console.WriteLine(converter.Convert(input));
 
-			//TREE
+            //TREE
 
-			//var tree = new ExpressionTree("ABC*EF-/+");
-			//var result = tree.Inorder();
+            //var tree = new ExpressionTree("ABC*EF-/+");
+            //var result = tree.Inorder();
 
-			//5
-			//var matrix = new Matrix<int>(10, 10);
+            //5
+            //var matrix = new Matrix<int>(10, 10);
+            //matrix.FillRandomly();
+            //var matrixOrderByCountOfEven = matrix.OrderBy(x => x.Count(_ => _ % 2 == 0)).Select(x => x.ToArray()).ToArray();
+            //for (var i = 0; i < matrixOrderByCountOfEven.Length; i++)
+            //{
+            //	for (var j = 0; j < matrixOrderByCountOfEven[i].Length; j++)
+            //	{
+            //		Console.Write(matrixOrderByCountOfEven[i][j] + " ");
+            //	}
+
+            //	Console.WriteLine();
+            //}
+
+            //Console.WriteLine(0 % 2 == 0);
+
+            //6.1
+     //       var matrix = new Matrix<int>(3, 3);
+     //       matrix.FillRandomly();
+     //       var ordered = matrix.OrderBy(row => row.Count(element => element.ToString().Where(Char.IsDigit).Select(ch => int.Parse(ch.ToString())).Sum() % 2 == 0));
+
+     //       for (var i = 0; i < matrix.RowsCount; i++)
+     //       {
+	    //        for (var j = 0; j < matrix.ColumnsCount; j++)
+	    //        {
+					//Console.Write($"{matrix[i, j]}\t");
+	    //        }
+
+	    //        Console.WriteLine();
+     //       }
+
+     //       Console.WriteLine(new string('-', 30));
+            
+     //       foreach (var row in ordered)
+     //       {
+	    //        foreach (var item in row)
+	    //        {
+		   //         Console.Write($"{item}\t");
+	    //        }
+	    //        Console.WriteLine();
+     //       }
+            
+            //6.2
+   //         var matrix = new Matrix<int>(3, 3);
 			//matrix.FillRandomly();
-			//var matrixOrderByCountOfEven = matrix.OrderBy(x => x.Count(_ => _ % 2 == 0)).Select(x => x.ToArray()).ToArray();
-			//for (var i = 0; i < matrixOrderByCountOfEven.Length; i++)
+
+			//var listMatrix = new List<List<int>>();
+			//for (var i = 0; i < matrix.RowsCount; i++)
+			//	listMatrix.Add(new List<int>(matrix.ElementAt(i)));
+
+			//var flag = false;
+			//for (var i = 0; i < listMatrix[0].Count; i++)
 			//{
-			//	for (var j = 0; j < matrixOrderByCountOfEven[i].Length; j++)
+			//	for (var j = 0; j < listMatrix.Count; j++)
 			//	{
-			//		Console.Write(matrixOrderByCountOfEven[i][j] + " ");
+			//		if (listMatrix[j][i] > 0 && listMatrix[j][i].IsPrime())
+			//		{
+			//			var min = listMatrix.Select(x => x.ElementAt(i)).Min();
+			//			var first = listMatrix.Select(x => x.ElementAt(i)).First();
+			//			var newElement = (int) Math.Pow(min, first);
+			//			listMatrix.Insert(i + 1, new List<int>(Enumerable.Range(0, listMatrix[0].Count).Select(x => 0).ToArray()));
+			//			listMatrix[i + 1][j] = newElement;
+			//			flag = true;
+			//			break;
+			//		}
+					
+			//	}
+			//	if (flag) break;
+			//}
+
+			//for (var i = 0; i < listMatrix.Count; i++)
+			//{
+			//	for (var j = 0; j < listMatrix[i].Count; j++)
+			//	{
+			//		Console.Write($"{listMatrix[i][j]}\t");
 			//	}
 
 			//	Console.WriteLine();
 			//}
 
-			//Console.WriteLine(0 % 2 == 0);
-
-			//6.1
-			//var matrix = new Matrix<int>(3, 3);
-			//matrix.FillRandomly();
-			//var ordered = matrix.OrderBy(x => x.Count(number => number.ToString()
-			//                                                          .Select(str => int.Parse(str.ToString())).Sum() % 2 == 0));
-
-			//6.2
-			var matrix = new Matrix<int>(3, 3);
-			matrix.FillRandomly();
-
-			Matrix<int> fucked;
-			for (var i = 0; i < matrix.ColumnsCount; i++)
-			{
-				for (var j = 0; j < matrix.RowsCount; j++)
-				{
-					if (matrix[j, i] >= 0 &&
-						matrix[j, i].IsPrime() &&
-						i != matrix.ColumnsCount - 1)
-					{
-						var min = matrix.Select(x => x.ElementAt(j)).Min();
-						var first = matrix.Select(x => x.ElementAt(j)).First();
-						var newElement = Math.Pow(min, first);
-						//TODO: доделать 6.2
-						//Добавить в каждом столбце после первого простого элемента, значение,
-						//равное минимальному элементу столбца, возведенному в степень, равную
-						//первому элементу.
-						break;
-					}
-				}
-			}
 		}
 
 		private static int Sum(int[] array) => InternalSumRecursion(array, 0, 0);
