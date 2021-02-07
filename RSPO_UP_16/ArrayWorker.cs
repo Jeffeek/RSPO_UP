767a5acc3201cae244a0dd4295848e13b78569d9
Mikhail Ilyushin <ilyushin_misha@mail.ru>
2021-02-07 23:02:45 +0300

    Проработка 16

---
+++ RSPO_UP_16/ArrayWorker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace RSPO_UP_16
+{
+    public class ArrayWorker
+    {
+        //удаление n первых элементов
+        public int[] RemoveRangeAtStart(int[] array, int N)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException(nameof(array));
+            if (array.Length < N) throw new ArgumentException(nameof(N));
+            var newArray = new int[array.Length - N];
+            if (array.Length - N == 0) return Array.Empty<int>();
+            Array.Copy(array, N, newArray, 0, array.Length - N);
+            return newArray;
+        }
+
+        //изменение всех элементов путем целочисленного деления на n
+        public int[] ChangeByDivOn(int[] array, int N)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException(nameof(array));
+            return array.Select(x => x / N).ToArray();
+        }
+
+        //поменять местами максимальный и nй элемент массива
+        public int[] SwapMaxAndElementAt(int[] array, int N)
+        {
+            if (ReferenceEquals(array, null)) throw new ArgumentNullException(nameof(array));
+            if (N <= 0) throw new ArgumentException(nameof(N));
+            var index = N - 1;
+            if (index >= array.Length - 1) throw new ArgumentException(nameof(index));
+            var indexOfMax = Array.FindIndex(array, x => x == array.Max());
+            var temp = array[indexOfMax];
+            array[indexOfMax] = array[index];
+            array[index] = temp;
+            return array;
+        }
+    }
+}
---
+++ RSPO_UP_16/ConsoleMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSPO_UP_16
+{
+	public delegate int[] ArrayWorkerDelegate(int[] array, int N);
+	public delegate void ArrayEvent(int[] array, bool flag);
+    public class ConsoleMenu
+    {
+	    private ArrayWorkerDelegate _arrayWorker;
+		private event ArrayEvent _ele
+	    private int[] _initialArray;
+	    public ConsoleMenu()
+	    {
+		    var worker = new ArrayWorker();
+		    _arrayWorker += worker.RemoveRangeAtStart;
+		    _arrayWorker += worker.ChangeByDivOn;
+		    _arrayWorker += worker.SwapMaxAndElementAt;
+	    }
+
+		public void Start()
+		{
+
+		}
+
+
+		private int SetStrategy()
+		{
+
+		}
+    }
+}
