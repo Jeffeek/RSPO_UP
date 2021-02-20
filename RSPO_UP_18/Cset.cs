9144d5900e2bea2617480379189131090d420792
Mikhail Ilyushin <ilyushin_misha@mail.ru>
2021-02-20 20:52:12 +0300

    19 full

---
+++ RSPO_UP_18/Cset.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace RSPO_UP_18
+{
+    public delegate void CSetElementAdded<T>(Cset<T> set, T item) where T : IEquatable<T>;
+
+    public sealed class Cset<T> : ICollection<T>, IEquatable<Cset<T>> where T : IEquatable<T>
+    {
+        private int _capacity;
+        private T[] _innerArray;
+
+        public event CSetElementAdded<T> ItemAdded;
+
+        public Cset() : this(4) { }
+
+        public Cset(int capacity)
+        {
+            if (capacity <= 0) throw new ArgumentOutOfRangeException(nameof(capacity));
+
+            _capacity = capacity;
+            _innerArray = new T[capacity];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < Count; i++) yield return _innerArray[i];
+        }
+
+        ~Cset()
+        {
+            Clear();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public Cset(IEnumerable<T> source)
+        {
+            var tempArray = source.ToArray();
+            var count = tempArray.Length;
+            _capacity = count;
+            Count = count;
+            _innerArray = new T[count];
+            for (var i = 0; i < count; i++) 
+                _innerArray[i] = tempArray[i];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count) throw new ArgumentOutOfRangeException(nameof(index));
+
+                return _innerArray[index];
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (Contains(item)) return;
+
+            if (Count + 1 > _capacity)
+            {
+                _capacity *= 2;
+                var tempArray = new T[_capacity];
+                Array.Copy(_innerArray, tempArray, Count);
+                _innerArray = tempArray;
+            }
+
+            _innerArray[Count] = item;
+            Count++;
+            ItemAdded?.Invoke(this, item);
+        }
+
+        public void Clear()
+        {
+            _capacity = 4;
+            _innerArray = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            for (var i = 0; i < Count; i++)
+                if (_innerArray[i].Equals(item))
+                    return true;
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex) => throw new NotImplementedException();
+
+        public bool Remove(T item)
+        {
+            if (!Contains(item)) return false;
+
+            var list = new List<T>(_innerArray);
+            list.Remove(item);
+            list.CopyTo(0, _innerArray, 0, list.Count);
+
+            return true;
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly => true;
+
+        public static bool operator *(Cset<T> first, Cset<T> second)
+        {
+            if (first.Count != second.Count) return false;
+
+            return !first.Where((t, i) => !t.Equals(second[i])).Any();
+        }
+
+        public static Cset<int> operator +(Cset<T> source, int item)
+        {
+            if (source.GetType().GenericTypeArguments[0].FullName != "System.Int32") throw new ArgumentException(nameof(source));
+
+            if (source.Contains((dynamic)item)) return null;
+
+            source.Add((dynamic)item);
+
+            return (dynamic)source;
+        }
+
+        public static bool operator ==(Cset<int> first, Cset<T> second) => first?.Equals(second) ?? second?.Equals(first) ?? true;
+
+        public static bool operator !=(Cset<int> first, Cset<T> second) => !(first == second);
+
+        public bool Equals(Cset<T> other)
+        {
+            if (Count != other?.Count) return false;
+
+            for (var i = 0; i < Count; i++)
+                if (!this[i].Equals(other[i]))
+                    return false;
+
+            return true;
+        }
+
+        public Cset<T> Difference(Cset<T> other)
+        {
+            var enumerable = this.Except(other);
+
+            return new Cset<T>(enumerable);
+        }
+
+        public override bool Equals(object obj) => ReferenceEquals(this, obj) || obj is Cset<T> other && Equals(other);
+
+        public override int GetHashCode() => HashCode.Combine(_capacity, _innerArray, Count);
+    }
+}
---
+++ RSPO_UP_19/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
+
+namespace RSPO_UP_19
+{
+    /*
+     * 1. Сформировать файл из значений случайных величин:
+       0,324; 0,524; 0,789; 0,556; 0,761; 0,248; 0,345; 0,911; 0,216, // +
+       Определить для данной последовательности среднее арифметическое компонентов,
+       значения которых меньше 0,5. // +
+     * 2. Разбить произвольный текст, находящийся в файле, на строки определенной
+       длины. При переносе слова предусмотреть вывод дефиса. // +
+     * 3. Сформировать файл целых чисел. Вывести на экран только четные значения
+       компонентов файла. // +
+     * 4. Из текста, расположенного в файле, исключить группы символов,
+       расположенных между круглыми скобками. // +
+     */
+
+    internal static class Program
+    {
+        #region FirstTaskFields
+
+        private static readonly string _firstTaskFilePath = $"{Directory.GetCurrentDirectory()}\\firstTaskIO_Nums.txt";
+
+        private static readonly double[] _firstTaskNumbers =
+        {
+            0.324, 0.524, 0.789, 0.556, 0.761, 0.248, 0.345, 0.911, 0.216
+        };
+
+        #endregion
+
+        #region SecondTaskFields
+
+        private static readonly string _secondTaskRandomText = $"sfsdfsdf fdsdfsdfsdfsdfsdf sdfhrfjyuj jdf jsl{Environment.NewLine}kdnfj ns jsdnf jnsdfj n jsdn jsnd{Environment.NewLine}fjnsd sd djndjsn sjdfns{Environment.NewLine}jdfnjsdn sdf fsdffsndf";
+
+        private static readonly string _secondTaskFilePath = $"{Directory.GetCurrentDirectory()}\\secondTask_randomText.txt";
+
+        #endregion
+
+        #region ThirdTaskFields
+
+        private static readonly Random _random = new Random();
+
+        private static readonly int[] _thirdTaskRandomNumbers = Enumerable.Range(0, 100)
+                                                                          .Select(x => _random.Next(-1000, 1000))
+                                                                          .ToArray();
+
+        private static readonly string _thirdTaskFilePath = $"{Directory.GetCurrentDirectory()}\\thirdTask_Numbers.txt";
+
+        #endregion
+
+        #region FourthTaskFields
+
+        private static readonly string _fourthTaskTextWithBrackets = "sdf sdfsdf(sdf),,sd()slokl(sdggsd);sd,42sadf2(sdf)f";
+
+        private static readonly string _fourthTaskFilePath = $"{Directory.GetCurrentDirectory()}\\fourthTask_TextWithBrackets.txt";
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region First
+
+            Console.WriteLine($"{new String('-', 10)} FirstTask {new String('-', 10)}");
+            InitializeFile();
+            CalculateAndPrint();
+
+            #endregion
+
+            #region Second
+
+            Console.WriteLine($"{new String('-', 10)} SecondTask {new String('-', 10)}");
+            WriteRandomTextToFile();
+            DoSecondTaskWork();
+
+            #endregion
+
+            #region Third
+
+            Console.WriteLine($"{new String('-', 10)} ThirdTask {new String('-', 10)}");
+            WriteRandomNumbersToFile();
+            ReadAndWriteOnlyEvenNumbers();
+
+            #endregion
+
+            #region Fourth
+
+            Console.WriteLine($"{new String('-', 10)} FourthTask {new String('-', 10)}");
+            InitializeFileWithTextWithBrackets();
+            RemoveAllTextBetweenBracketsAndPrint();
+
+            #endregion
+        }
+
+        #region FirstTask
+
+        private static void InitializeFile()
+        {
+            var textToWrite = String.Join(' ', _firstTaskNumbers);
+            File.WriteAllText(_firstTaskFilePath, textToWrite);
+        }
+
+        private static double[] ReadNumbersReadFromFile() =>
+            File.ReadAllText(_firstTaskFilePath)
+                .Split()
+                .Select(Double.Parse)
+                .ToArray();
+
+        private static void CalculateAndPrint()
+        {
+            var numbers = ReadNumbersReadFromFile();
+            var avg = numbers.Where(x => x > 0.5).Average();
+            Console.WriteLine($"Average of numbers {avg}");
+        }
+
+        #endregion
+
+        #region SecondTask
+
+        private static void WriteRandomTextToFile() => 
+            File.WriteAllText(_secondTaskFilePath, _secondTaskRandomText);
+
+        private static string ReadAllTextFromFile() => File.ReadAllText(_secondTaskFilePath);
+
+        private static void DoSecondTaskWork()
+        {
+            var text = ReadAllTextFromFile();
+            var lastIndex = 0;
+
+            foreach (Match m in Regex.Matches(text, @".{5}"))
+            {
+                Console.WriteLine($"{m.Value} - {m.Value.Length}");
+                lastIndex = m.Index + m.Value.Length;
+            }
+
+            if (text.Length - lastIndex > 0)
+                Console.WriteLine(text[lastIndex..]);
+        }
+
+        #endregion
+
+        #region ThirdTask
+
+        private static void WriteRandomNumbersToFile()
+        {
+            var textToWrite = String.Join(' ', _thirdTaskRandomNumbers);
+            File.WriteAllText(_thirdTaskFilePath, textToWrite);
+        }
+
+        private static int[] ReadRandomNumbersFromFile() =>
+            File.ReadAllText(_thirdTaskFilePath)
+                .Split()
+                .Select(Int32.Parse)
+                .ToArray();
+
+        private static void ReadAndWriteOnlyEvenNumbers()
+        {
+            var numbers = ReadRandomNumbersFromFile();
+            var textToPrint = String.Join(Environment.NewLine, numbers.Where(x => x % 2 == 0));
+            Console.WriteLine(textToPrint);
+        }
+
+        #endregion
+
+        #region FourthTask
+
+        private static void InitializeFileWithTextWithBrackets() => File.WriteAllText(_fourthTaskFilePath, _fourthTaskTextWithBrackets);
+
+        private static string ReadTextWithBracketsFromFile() => File.ReadAllText(_fourthTaskFilePath);
+
+        private static void RemoveAllTextBetweenBracketsAndPrint()
+        {
+            var initialText = ReadTextWithBracketsFromFile();
+            Console.WriteLine($"Initial text: {initialText}");
+            var pattern = new Regex("\\(\\w*\\)");
+            var newText =  pattern.Replace(initialText, String.Empty);
+            Console.WriteLine($"New text: {newText}");
+        }
+
+        #endregion
+    }
+}
